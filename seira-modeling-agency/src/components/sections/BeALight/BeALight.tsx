05af357360fc4d07df6c076444b12ffc094a3b09
Medkabs <mohamedkaba0629@gmail.com>
2025-09-18 23:05:20 -0400

    v0.8

--- seira-modeling-agency/src/components/sections/BeALight/BeALight.tsx
+++ seira-modeling-agency/src/components/sections/BeALight/BeALight.tsx
@@ -1,22 +1,101 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./BeALight.css";
 
 const BeALight: React.FC = () => {
+  const headingRef = useRef<HTMLHeadingElement>(null);
+
+  useEffect(() => {
+    if (!headingRef.current) return;
+
+    const resolver = {
+      resolve: function (options: any, callback?: () => void) {
+        const resolveString = options.resolveString || options.element.getAttribute("data-target-resolver");
+        const combinedOptions = { ...options, resolveString };
+
+        function getRandomInteger(min: number, max: number) {
+          return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+
+        function randomCharacter(characters: string[]) {
+          return characters[getRandomInteger(0, characters.length - 1)];
+        }
+
+        function doRandomiserEffect(options: any, callback?: () => void) {
+          let { iterations, partialString, element, characters, timeout } = options;
+
+          setTimeout(() => {
+            if (iterations >= 0) {
+              const nextOptions = { ...options, iterations: iterations - 1 };
+
+              if (iterations === 0) {
+                element.textContent = partialString;
+              } else {
+                element.textContent =
+                  partialString.substring(0, partialString.length - 1) +
+                  randomCharacter(characters);
+              }
+
+              doRandomiserEffect(nextOptions, callback);
+            } else if (callback) {
+              callback();
+            }
+          }, timeout);
+        }
+
+        function doResolverEffect(options: any, callback?: () => void) {
+          const { resolveString, characters, offset } = options;
+          const partialString = resolveString.substring(0, offset);
+          const combinedOptions = { ...options, partialString };
+
+          doRandomiserEffect(combinedOptions, () => {
+            const nextOptions = { ...options, offset: offset + 1 };
+            if (offset <= resolveString.length) {
+              doResolverEffect(nextOptions, callback);
+            } else if (callback) callback();
+          });
+        }
+
+        doResolverEffect(combinedOptions, callback);
+      },
+    };
+
+    const strings = [
+      "BE A LIGHT",
+      "FOR GOD",
+      "BE A LIGHT",
+    ];
+
+    let counter = 0;
+
+    const options = {
+      offset: 0,
+      timeout: 30,
+      iterations: 10,
+      characters: [
+        "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z","#","%","&","-","+","_","?","/","\\","="
+      ],
+      resolveString: strings[counter],
+      element: headingRef.current,
+    };
+
+    function callback() {
+      setTimeout(() => {
+        counter++;
+        if (counter >= strings.length) counter = 0;
+        const nextOptions = { ...options, resolveString: strings[counter], offset: 0 };
+        resolver.resolve(nextOptions, callback);
+      }, 1000);
+    }
+
+    resolver.resolve(options, callback);
+  }, []);
+
   return (
-    <section className="be-a-light">
-      {/* Flickering glow background */}
-      <div className="light-glow"></div>
-
-      {/* Flickering lamp (⚪️) */}
-      <div className="lamp">
-        <h1 className="flicker-text">⚪️</h1>
-      </div>
-
-      {/* Reveal Word (independent of lamp) */}
-      <h2 className="reveal-word">BE <br /> A <br /> LIGHT</h2>
-    </section>
+    <div className="container">
+      <h1 className="heading" ref={headingRef} data-target-resolver></h1>
+    </div>
   );
 };
 
